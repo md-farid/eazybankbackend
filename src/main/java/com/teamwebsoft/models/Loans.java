c2075e439026fe773d7b86bb0ee92aff4e09baa9
md.farid <md.farid.ice@gmail.com>
2023-02-05 12:19:25 +0600

    loans entity created

---
+++ src/main/java/com/teamwebsoft/models/Loans.java
@@ -0,0 +1,46 @@
+package com.teamwebsoft.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "loans")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Loans {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "loan_number")
+    private long loanNumber;
+
+    @Column(name = "customer_id")
+    private long customerId;
+
+    @Column(name = "start_dt")
+    private Date startDt;
+
+    @Column(name = "loan_type")
+    private String loanType;
+
+    @Column(name = "total_loan")
+    private int totalLoan;
+
+    @Column(name = "amount_paid")
+    private int amountPaid;
+
+    @Column(name = "outstanding_amount")
+    private int outstandingAmount;
+
+    @Column(name = "created_at", updatable = false)
+    @CreationTimestamp
+    private Date createAt;
+}
