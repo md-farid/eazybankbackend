ca0d7996b1b6c96939be4209a36faaf82ddde13d
md.farid <md.farid.ice@gmail.com>
2023-02-05 18:18:18 +0600

    project security config completed

--- src/main/java/com/teamwebsoft/config/ProjectSecurityConfig.java
+++ src/main/java/com/teamwebsoft/config/ProjectSecurityConfig.java
@@ -3,20 +3,21 @@ package com.teamwebsoft.config;
 import com.teamwebsoft.filters.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
-import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Collection;
 import java.util.Collections;
 
-@Component
+@Configuration
 public class ProjectSecurityConfig {
 
     @Bean
@@ -47,4 +48,8 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
