8a669882ccb650a32e7945e21da04da8a562ccd0
md.farid <md.farid.ice@gmail.com>
2023-02-05 11:43:55 +0600

    startup project setup

---
+++ src/main/java/com/teamwebsoft/EazybankbackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamwebsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazybankbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EazybankbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamwebsoft/EazybankbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamwebsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazybankbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
